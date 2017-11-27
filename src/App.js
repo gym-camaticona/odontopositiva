4b0eea12c88f40db7249e016ef94e5c40efc1e10
GLADYSYOVANAMAMANICAMATICONA <gladysyovanamamanicamaticona@gmail.com>
2017-11-26 21:27:59 -0500

    gladys

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from "redux-zero/react";
 import logo from './logo.svg';
 import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+import Detalles from './detalles'
+const App = () => {
+  return (
+    <div>
+      <div> 
+        <Detalles/>
       </div>
-    );
-  }
+          </div >
+  );
 }
 
 export default App;
+
---
+++ src/detalles.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from "redux-zero/react";
+import { Row, Grid, Form, FormGroup, Col, ControlLabel, FormControl, Checkbox, Button } from 'react-bootstrap';
+import { HashRouter, Switch, Route, NavLink } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+//Componete para la vista de detalles de clinica//
+const Detalles = () => {
+    return (
+        <div>
+            <Row>
+                <Col md={12}>
+                    <h1 className="g-titulo">Detalles de Clínica</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={6}>
+                    <h1 className="g-titulo-detalle">Tratamientos de la Clínica</h1>
+                    <li>Ortodoncia</li>
+                </Col>
+                <Col md={6}>
+                    <h1 className="g-titulo-detalle">Cobertura del Seguro</h1>
+                    <li>Ortodoncia</li>
+                </Col>
+            </Row>
+        </div>
+    );
+}
+export default Detalles;
